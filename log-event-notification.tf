38e1d5f735df370b5b56ba072e9cb20b46f8a5ac
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-01-02 18:22:38 +0530

    dne

--- log-event-notification.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cloudwatch_log_subscription_filter" "log_group_subscription_filter" {
-  for_each        = toset(var.log_group_names)
-  name            = "Error-Info-Warn"
-  log_group_name  = each.key
-  filter_pattern  = var.subscription_filter_pattern
-  destination_arn = module.lambda.arn
-  distribution    = "Random"
-}
