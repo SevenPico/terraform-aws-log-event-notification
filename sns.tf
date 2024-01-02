cf9e1d5b2fef395528b933298b2cd8afe98993fa
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-01-02 19:46:33 +0530

    decrypt

--- sns.tf
+++ sns.tf
@@ -75,7 +75,7 @@ data "aws_iam_policy_document" "lambda_role_policy_doc" {
     ]
   }
   statement {
-    sid     = "AllowPub"
+    sid     = "AllowDecrypt"
     effect  = "Allow"
     actions = ["kms:GenerateDataKey","kms:Decrypt",]
     resources = [
