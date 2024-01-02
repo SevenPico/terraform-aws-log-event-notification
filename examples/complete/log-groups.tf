c634d8a877d58c0cfd204731e742930f9c0abb43
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-01-02 17:37:41 +0530

    removed curlies

--- examples/complete/log-groups.tf
+++ examples/complete/log-groups.tf
@@ -1,13 +1,13 @@
 resource "aws_cloudwatch_log_group" "log_group_1" {
   count = module.context.enabled ? 1 : 0
-  name = "/aws/example/${module.context.id}/abc}"
+  name = "/aws/example/${module.context.id}/abc"
 
   tags = module.context.tags
 }
 
 resource "aws_cloudwatch_log_group" "log_group_2" {
   count = module.context.enabled ? 1 : 0
-  name = "/aws/example/${module.context.id}/efg}"
+  name = "/aws/example/${module.context.id}/efg"
 
   tags = module.context.tags
 }
