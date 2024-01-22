6feaed04709bf88cd59b10b6c0fcc3cd7a67a791
Jimish Fotariya <fotariyajimish@gmail.com>
2024-01-22 20:00:08 +0530

    fix: checkov ignore

--- examples/complete/log-groups.tf
+++ examples/complete/log-groups.tf
@@ -1,21 +1,32 @@
 resource "aws_cloudwatch_log_group" "log_group_1" {
+  #checkov:skip=CKV_AWS_338: "Ensure CloudWatch log groups retains logs for at least 1 year"
+  #checkov:skip=CKV_AWS_158: "Ensure that CloudWatch Log Group is encrypted by KMS"
+
   count = module.context.enabled ? 1 : 0
   name  = "/aws/example/${module.context.id}/abc"
+  retention_in_days = 7
 
   tags = module.context.tags
 }
 
 resource "aws_cloudwatch_log_group" "log_group_2" {
+  #checkov:skip=CKV_AWS_338: "Ensure CloudWatch log groups retains logs for at least 1 year"
+  #checkov:skip=CKV_AWS_158: "Ensure that CloudWatch Log Group is encrypted by KMS"
+
   count = module.context.enabled ? 1 : 0
   name  = "/aws/example/${module.context.id}/efg"
+  retention_in_days = 7
 
   tags = module.context.tags
 }
 
 resource "aws_cloudwatch_log_group" "log_group_3" {
+  #checkov:skip=CKV_AWS_338: "Ensure CloudWatch log groups retains logs for at least 1 year"
+  #checkov:skip=CKV_AWS_158: "Ensure that CloudWatch Log Group is encrypted by KMS"
+
   count = module.context.enabled ? 1 : 0
   name  = "/aws/example/${module.context.id}/hij"
-
+  retention_in_days = 7
   tags = module.context.tags
 }
 
